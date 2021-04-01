3834a2569e440ecc394eefde3d97f4683947982d
Marilyn Greve majel45@icloud.com <majel45@icloud.com>
2021-04-01 10:26:02 +0200

    added react logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import logo from "./logo.svg";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"> Hello
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
